b02f69f08afd65dfd4fea6b4f7b0bd33d2a74951
Kushal Pandya <pandyak@uoguelph.ca>
2019-09-25 21:53:12 -0400

    initial commit

---
+++ Last.fm/Last.fm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Last.fm
+//
+//  Created by Kushal Pandya on 2019-09-25.
+//  Copyright © 2019 Kushal Pandya. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Last.fm/Last.fmTests/Last_fmTests.swift
@@ -0,0 +1,34 @@
+//
+//  Last_fmTests.swift
+//  Last.fmTests
+//
+//  Created by Kushal Pandya on 2019-09-25.
+//  Copyright © 2019 Kushal Pandya. All rights reserved.
+//
+
+import XCTest
+@testable import Last_fm
+
+class Last_fmTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
